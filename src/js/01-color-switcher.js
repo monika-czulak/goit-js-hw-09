3fe96a5b2c9926bdc975931d3c65886c9b6ae598
monika-czulak <czulak.monika@gmail.com>
2022-12-27 21:33:19 +0100

    01-color-switcher completed

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,2 +1,20 @@
 const btnStart = document.querySelector('[data-start]');
 const btnStop = document.querySelector('[data-stop]');
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+btnStart.addEventListener('click', () => {
+  timer = setInterval(() => {
+    document.body.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+  btnStart.disabled = true;
+  btnStop.disabled = false;
+});
+
+btnStop.addEventListener('click', () => {
+  clearInterval(timer);
+  btnStart.disabled = false;
+  btnStop.disabled = true;
+});

3f4c41d73d33a25138847d86197909a65001289f
monika-czulak <109820128+monika-czulak@users.noreply.github.com>
2022-12-27 20:33:46 +0000

    Deploying to gh-pages from @ monika-czulak/goit-js-hw-09@3fe96a5b2c9926bdc975931d3c65886c9b6ae598 🚀

---
+++ 01-color-switcher.d2b732e0.js
@@ -0,0 +1,2 @@
+const e=document.querySelector("[data-start]"),t=document.querySelector("[data-stop]");e.addEventListener("click",(()=>{timer=setInterval((()=>{document.body.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16)}`}),1e3),e.disabled=!0,t.disabled=!1})),t.addEventListener("click",(()=>{clearInterval(timer),e.disabled=!1,t.disabled=!0}));
+//# sourceMappingURL=01-color-switcher.d2b732e0.js.map

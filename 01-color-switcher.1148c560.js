eebfe641e0745f0b1e25debaaa7a643e4fff4420
monika-czulak <109820128+monika-czulak@users.noreply.github.com>
2022-12-29 15:13:44 +0000

    Deploying to gh-pages from @ monika-czulak/goit-js-hw-09@dc314cac7769ace60f586573e960d76ae926518b 🚀

--- 01-color-switcher.1148c560.js
+++
@@ -1,2 +0,0 @@
-const e=document.querySelector("[data-start]"),t=document.querySelector("[data-stop]");t.disabled=!0,e.addEventListener("click",(()=>{timer=setInterval((()=>{document.body.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16)}`}),1e3),e.disabled=!0,t.disabled=!1})),t.addEventListener("click",(()=>{clearInterval(timer),e.disabled=!1,t.disabled=!0}));
-//# sourceMappingURL=01-color-switcher.1148c560.js.map
--- 01-color-switcher.a92b8a22.js
+++
@@ -1,2 +0,0 @@
-!function(){var t=document.querySelector("[data-start]"),e=document.querySelector("[data-stop]");e.disabled=!0,t.addEventListener("click",(function(){timer=setInterval((function(){document.body.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16))}),1e3),t.disabled=!0,e.disabled=!1})),e.addEventListener("click",(function(){clearInterval(timer),t.disabled=!1,e.disabled=!0}))}();
-//# sourceMappingURL=01-color-switcher.a92b8a22.js.map

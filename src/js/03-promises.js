0536c482846ae26becfaca1ed4dc2273442848d1
monika-czulak <czulak.monika@gmail.com>
2022-12-30 21:28:34 +0100

    03-promises completed

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,39 @@
-function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
+import { Notify } from 'notiflix';
+
+let delay = document.querySelector('input[name="delay"]');
+let step = document.querySelector('input[name="step"]');
+let amount = document.querySelector('input[name="amount"]');
+const submit = document.querySelector('form');
+
+const createPromise = (position, delay) => {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      const shouldResolve = Math.random() > 0.3;
+      if (shouldResolve) {
+        resolve({ position, delay });
+      } else {
+        reject({ position, delay });
+      }
+    }, delay);
+  });
+};
+
+const handleSubmit = e => {
+  e.preventDefault();
+  let delayValue = Number(delay.value);
+  let stepValue = Number(step.value);
+  let amountValue = Number(amount.value);
+
+  for (let i = 1; i <= amountValue; i++) {
+    createPromise(i, delayValue)
+      .then(({ position, delay }) => {
+        Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
+      })
+      .catch(({ position, delay }) => {
+        Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
+      });
+    delayValue += stepValue;
   }
-}
+};
+
+submit.addEventListener('submit', handleSubmit);
